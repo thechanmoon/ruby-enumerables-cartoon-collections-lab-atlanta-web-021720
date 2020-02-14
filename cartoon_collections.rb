cd5500b44a522f9006472b47963b49cde0e85975
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 18:19:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,4 +18,8 @@ end
 def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  ingredients.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
